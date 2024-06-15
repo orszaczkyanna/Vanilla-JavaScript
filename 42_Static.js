f2c509cf9334a2f367aa0e58df43da1cbe6f43a9
orszaczkyanna <orszaczky.anna@gmail.com>
2024-06-15 15:39:01 +0200

    Static

---
+++ 42_Static.js
@@ -0,0 +1,54 @@
+// static = keyword that defines properties or methods that belong
+//          to a class itself rather than the objects created
+//          from that class (class owns anything static, not the objects)
+
+class MathUtil {
+    static PI = 3.14159;
+
+    static getDiameter(radius) {
+        return radius * 2;
+    }
+
+    static getCircumference(radius) {
+        return 2 * this.PI * radius; // this.PI = MathUtil.PI
+    }
+
+    static getArea(radius) {
+        return this.PI * radius * radius;
+    }
+
+}
+
+console.log(MathUtil.PI);
+console.log(MathUtil.getDiameter(10));
+console.log(MathUtil.getCircumference(10));
+console.log(MathUtil.getArea(10));
+
+// ----------------------------------------------------------------
+
+class User {
+    static userCount = 0;
+
+    constructor(username) {
+        this.username = username;
+        User.userCount++;
+    }
+
+    static getUserCount() {
+        console.log(`There are ${this.userCount} users online`); // this.userCount = User.userCount
+    }
+
+    sayHello() {
+        console.log(`Hello, my username is ${this.username}`); // this.username = user1.username
+    }
+}
+
+const user1 = new User("Spongebob");
+const user2 = new User("Patrick");
+const user3 = new User("Sandy");
+
+user1.sayHello();
+user2.sayHello();
+user3.sayHello();
+
+User.getUserCount();
