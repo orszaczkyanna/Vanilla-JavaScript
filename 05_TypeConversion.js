e8fcf5d448e4054c119f45788dbd9ec6bfb30299
orszaczkyanna <orszaczky.anna@gmail.com>
2024-06-30 16:14:13 +0200

    TypeConversion

---
+++ 05_TypeConversion.js
@@ -0,0 +1,12 @@
+// type conversion = change the datatype of a value to another
+//                   (strings, numbers, booleans)
+
+let age = "18";
+let username = 65;
+let isStudent = "false";
+
+age = Number(age);
+username = String(username);
+isStudent = Boolean(isStudent);
+
+console.log(typeof isStudent);
