a4858e3dbca16dd0278041b020d44787e51d735f
orszaczkyanna <orszaczky.anna@gmail.com>
2024-07-02 07:29:05 +0200

    Calculator

---
+++ 62_Calculator/main.js
@@ -0,0 +1,23 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+        // IMPORTANT NOTE
+        // eval() allows you to execute a string of code as if it were actual JavaScript code.
+        // While it is a useful tool, it is a security risk.
+        // Use eval() ONLY for learning purposes and not any actual production code.
+    }
+    catch(error){
+        console.error(error);
+        display.value = "Error";
+    }
+}
