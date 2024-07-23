0da915751a676af5df4bd1419e2bc53abfb8de61
orszaczkyanna <orszaczky.anna@gmail.com>
2024-07-23 21:07:03 +0200

    WeatherApp

--- 80_WeatherApp/main.js
+++ 80_WeatherApp/main.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "***REMOVED***";
+const apiKey = "";
 
 weatherForm.addEventListener("submit", async event => {
     
